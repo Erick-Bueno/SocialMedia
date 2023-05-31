1b86a878696496d1f140ed77e8df18151faf4291
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-05-30 21:15:58 -0300

    Feat: index field email implemented

--- src/backend/data/map/UserMap.cs
+++ src/backend/data/map/UserMap.cs
@@ -6,31 +6,32 @@ public class UserMap : IEntityTypeConfiguration<UserModel>
     public void Configure(EntityTypeBuilder<UserModel> builder)
     {
         builder.HasKey(u => u.id);
-        builder.Property(u => u.Email).IsRequired().HasMaxLength(200);
-        builder.Property(u => u.Password).IsRequired().HasMaxLength(600);
-        builder.Property(u => u.Telephone).IsRequired();
-        builder.Property(u=>u.User_Photo).HasMaxLength(600);
-        builder.Property(u=>u.UserName).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.email).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.password).IsRequired().HasMaxLength(600);
+        builder.Property(u => u.telephone).IsRequired();
+        builder.Property(u => u.userPhoto).HasMaxLength(600);
+        builder.Property(u => u.userName).IsRequired().HasMaxLength(200);
+        builder.HasIndex(u => u.email);
         builder.HasMany(u => u.posts)
         .WithOne(p => p.user)
-        .HasForeignKey(p=> p.User_id);
+        .HasForeignKey(p => p.userId);
         builder.HasMany(u => u.userlikes)
             .WithOne(l => l.userModel)
-            .HasForeignKey(l => l.Users_id);
+            .HasForeignKey(l => l.userId);
 
         builder.HasMany(f => f.UsersFriends).
             WithOne(u => u.userModel)
-            .HasForeignKey(f => f.user_id);
+            .HasForeignKey(f => f.userId);
 
         builder.HasMany(f => f.UsersFriends2)
             .WithOne(u => u.userModel2)
-            .HasForeignKey(f => f.user_id_2);
+            .HasForeignKey(f => f.userId2);
 
         builder.HasMany(r => r.usersRequests)
             .WithOne(u => u.userModel)
-            .HasForeignKey( u => u.Requester_id);
+            .HasForeignKey(u => u.requesterId);
         builder.HasMany(r => r.usersRequests2)
             .WithOne(u => u.UserModel2)
-            .HasForeignKey(u => u.Receiver_id);    
+            .HasForeignKey(u => u.receiverId);
     }
 }
